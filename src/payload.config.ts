dada69e4446a21d997a7eecf907d181438bfabcf
Bendix Behn <>
2025-12-29 23:00:28 +0100

    set favicon

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -72,6 +72,7 @@ export default buildConfig({
         media: true,
       },
       token: process.env.BLOB_READ_WRITE_TOKEN || '',
+      clientUploads: true,
     }),
   ],
   globals: [Header, Footer],
