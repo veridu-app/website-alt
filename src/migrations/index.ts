2095de3becb21318121f9d277a164d6ed06ebcf1
Bendix Behn <>
2025-12-30 15:07:22 +0100

    add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -10,6 +10,7 @@ import * as migration_20251230_115546_add_background_color_to_info_cards from '.
 import * as migration_20251230_122426_add_catch_info_and_footnotes_to_info_cards from './20251230_122426_add_catch_info_and_footnotes_to_info_cards'
 import * as migration_20251230_133127_add_catch_info_position_and_banner_color from './20251230_133127_add_catch_info_position_and_banner_color'
 import * as migration_20251230_145406_add_view_all_link_to_archive_block from './20251230_145406_add_view_all_link_to_archive_block'
+import * as migration_20251230_180000_fix_footnotes_table_structure from './20251230_180000_fix_footnotes_table_structure'
 
 export const migrations = [
   {
@@ -71,5 +72,10 @@ export const migrations = [
     up: migration_20251230_145406_add_view_all_link_to_archive_block.up,
     down: migration_20251230_145406_add_view_all_link_to_archive_block.down,
     name: '20251230_145406_add_view_all_link_to_archive_block',
+  },
+  {
+    up: migration_20251230_180000_fix_footnotes_table_structure.up,
+    down: migration_20251230_180000_fix_footnotes_table_structure.down,
+    name: '20251230_180000_fix_footnotes_table_structure',
   },
 ]
