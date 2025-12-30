211d213dee038b8d7ecabf498b12ac21eb1d9541
Bendix Behn <>
2025-12-30 08:55:04 +0100

    update migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251230_075229 from './20251230_075229';
+
+export const migrations = [
+  {
+    up: migration_20251230_075229.up,
+    down: migration_20251230_075229.down,
+    name: '20251230_075229'
+  },
+];
